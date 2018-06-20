bee162b6b84cbdf19767f5d2a509b83f8d88107f
Stark <853095099@qq.com>
2018-06-20 23:53:26 +0800

    treePlot

--- ch03/trees.py
+++ ch03/trees.py
@@ -1,4 +1,5 @@
 from math import log
+import operator
 
 def calcShannonEnt(dataSet):
     """
@@ -45,6 +46,11 @@ def splitDataSet(dataSet, axis, value):
     return retDataSet
 
 def chooseBestFeatureToSplit(dataSet):
+    """
+    找到最佳特征
+    :param dataSet: 数据集
+    :return:
+    """
     numFeatures = len(dataSet[0]) - 1
     baseEntropy = calcShannonEnt(dataSet)
     bestInfoGain = 0.0; bestFeature = -1
@@ -61,3 +67,36 @@ def chooseBestFeatureToSplit(dataSet):
                 bestInfoGain = infoGain
                 bestFeature = i
     return bestFeature
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet, labels):
+    """
+    构建决策树
+    :param dataSet: 数据集
+    :param labels: 标签列表
+    :return:
+    """
+    classList = [example[-1] for example in dataSet]
+    # 所有类标签全部相同 直接返回该类标签
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    # 使用完了所有特征 仍然不能将数据集划分成仅包含唯一类别的类标签
+    if len( [0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet) # 选择最佳特征
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for values in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][values] = createTree(splitDataSet(dataSet, bestFeat, values), subLabels)
+    return myTree
